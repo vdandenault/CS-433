35be831c87d8d7225747a984fb6ff7cbdf651b87
fabnem12 <fabnem12@gmail.com>
2022-10-25 14:13:03 +0200

    implementations fixed for fabrice's functions

--- project1/implementations.py
+++ project1/implementations.py
@@ -25,6 +25,10 @@ def predict_logistic_regression(X, w):
 def predict(tx, w):
     return tx.T @ w
 
+def compute_loss(y, tx, w): #loss for least squares
+    N = y.shape[0]
+    return (1/(2*N)) * sum((y[i] - tx[i].T @ w) ** 2  for i in range(N))
+
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
     """The Gradient Descent (GD) algorithm.
         
@@ -41,38 +45,21 @@ def least_squares_GD(y, tx, initial_w, max_iters, gamma):
     """
 
     def compute_gradient_least_squares(y, tx, w):
-        """Computes the gradient at w.
-            
-        Args:
-            y: numpy array of shape=(N, )
-            tx: numpy array of shape=(N,2)
-            w: numpy array of shape=(2, ). The vector of model parameters.
-            
-        Returns:
-            An numpy array of shape (2, ) (same shape as w), containing the gradient of the loss at w.
-        """
-        
         N = y.shape[0]
-
-        return - (1/N) * sum((y[i] - tx[i].T @ w) ** 2  for i in range(N))
+        return -(1/N) * sum(tx[i].T * (y[i] - tx[i].T @ w) for i in range(N)) #formula
 
     # Define parameters to store w and loss
-    ws = [initial_w]
-    losses = []
     w = initial_w
     for n_iter in range(max_iters):
-        grad = compute_gradient_least_squares(y, tx, w)
-        loss = compute_loss(y, tx, w)
+        grad = compute_gradient_least_squares(y, tx, w) #compute the gradient for the descent
+        loss = compute_loss(y, tx, w) #compute the loss to show it
         
         w = w - gamma * grad
-        
-        # store w and loss
-        ws.append(w)
-        losses.append(loss)
+
         print("GD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
               bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
 
-    return ws[-1], losses[-1]
+    return w, loss
 
 def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
     """The Stochastic Gradient Descent (SGD) algorithm.
@@ -102,15 +89,6 @@ def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
         """
         
         def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
-            """
-            Generate a minibatch iterator for a dataset.
-            Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
-            Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
-            Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
-            Example of use :
-            for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
-                <DO-SOMETHING>
-            """
             data_size = len(y)
 
             if shuffle:
@@ -127,15 +105,12 @@ def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
                 if start_index != end_index:
                     yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
 
-        N = y.shape[0]
+        #prepare a random mini-batch of train data, the gradient will be computed only for this subset
         yStoch, txStoch = next(batch_iter(y, tx, batch_size))
-        e = sum((yStoch[i] - txStoch[i].T @ w) ** 2  for i in range(N))
-        
-        return -(1/N) * txStoch.T @ sum((yStoch[i] - txStoch[i].T @ w) ** 2  for i in range(N))
+        N = yStoch.shape[0]
+        return -(1/N) * sum(txStoch[i].T * (yStoch[i] - txStoch[i].T @ w) for i in range(N))
 
     # Define parameters to store w and loss
-    ws = [initial_w]
-    losses = []
     w = initial_w
     
     for n_iter in range(max_iters):
@@ -144,14 +119,11 @@ def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
         
         w = w - gamma * grad
         
-        # store w and loss
-        ws.append(w)
-        losses.append(loss)
 
         print("SGD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
               bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
     
-    return ws[-1], losses[-1]
+    return w, loss
 
 def least_squares(y, tx):
     """Calculate the least squares solution.
