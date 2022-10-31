fa33e7b65837468139b88a13d4baca077ad0fbd0
Vincent Dandenault <vdandenault@gmail.com>
2022-10-31 06:40:22 +0100

    fix file name

--- project1/run.py
+++ project1/run.py
@@ -11,7 +11,7 @@ def main():
     initial_w = np.zeros((input_data_train.shape[1],1))
     w, losses = logistic_regression(yb_train, input_data_train, initial_w=initial_w, max_iters=max_iters, gamma=gamma)
     labels = predict_logistic_regression(w, input_data_test)
-    create_csv_submission(ids=ids_test, y_pred=labels, name="Results/Submission_least_squares_100.csv")
+    create_csv_submission(ids=ids_test, y_pred=labels, name="Results/Submission_logisitic_regression_1000.csv")
     plot_loss_function(losses, max_iters)
 
 if __name__ == "__main__":
